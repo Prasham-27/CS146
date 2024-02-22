06d027230383a069b5024ad5df46e2d3ddc6a96d
Prasham-27 <prashams27@gmail.com>
2024-02-21 23:55:29 -0800

    .

---
+++ HW/HW6/Main.java
@@ -0,0 +1,31 @@
+package HW.HW6;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-5,0,5,10,-10,0};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                //two pointers approach
+                int lo = i+1, hi = nums.length-1, sum = 0 - nums[i];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (nums[lo] + nums[hi] < sum) lo++;
+                    else hi--;
+                }
+            }
+        }
+        return res;
+    }
+}
+  
